546795d4a0139108733a6e16d6ae29140b176a5c
daniel080395 <jonmundsson@hotmail.com>
2020-01-30 16:57:58 +0100

    new

--- Labb1/Movable.java
+++ Labb1/Movable.java
@@ -1,5 +1,16 @@
 public interface Movable {
-    void Move();
-    void turnRight();
+    /**
+     * Method move moves a car
+     */
+    void move();
+
+    /**
+     * method turnLeft turns a car left
+     */
     void turnLeft();
+
+    /**
+     * method turnRight turns a car right
+     */
+    void turnRight();
 }
