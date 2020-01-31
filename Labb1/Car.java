7dfb0728e7a45b28f0893edfa8fd3f038b7f2a81
olleWesterlund <60099045+olleWesterlund@users.noreply.github.com>
2020-01-31 16:41:38 +0100

    Fixed setSpeedFactor

--- Labb1/Car.java
+++ Labb1/Car.java
@@ -262,6 +262,12 @@ public abstract class Car implements Movable {
      * @param currentSpeed holds the new currentSpeed as a double
      */
     protected void setCurrentSpeed(double currentSpeed) {
-        this.currentSpeed = currentSpeed;
+        if (currentSpeed > this.getEnginePower()) {
+            this.currentSpeed = this.getEnginePower();
+        } else if (currentSpeed < 0) {
+            this.currentSpeed = 0;
+        } else {
+            this.currentSpeed = currentSpeed;
+        }
     }
 }
