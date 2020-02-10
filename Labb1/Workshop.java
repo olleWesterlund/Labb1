270f411756491e671777ce9eacc5fb6f741f66d6
olleWesterlund <60099045+olleWesterlund@users.noreply.github.com>
2020-02-10 15:06:01 +0100

    Added stuff

--- Labb1/Workshop.java
+++ Labb1/Workshop.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class Workshop <T extends Car>{
-
+public class Workshop<T extends Car> {
     private int maxNrOfCars;
-    private List<T> carsInWorkshop;
+    protected List<T> carsInWorkshop = new ArrayList<T>();
 
     public Workshop(int maxNrOfCars) {
         this.maxNrOfCars = maxNrOfCars;
+
     }
 
     public void putCarInWorkshop(T car) {
-
+        if (carsInWorkshop.size() < maxNrOfCars) {
+            carsInWorkshop.add(car);
+        } else {
+            throw new IllegalArgumentException("Workshop is full");
+        }
     }
+
+    public void getCarFromWorkshop(T car) {
+        carsInWorkshop.remove(car);
+    }
+
+
 
 }
